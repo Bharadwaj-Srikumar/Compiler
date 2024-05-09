ea73d6d514d23efacf1ea1a18bf53176c01080bc
Jessieca29 <zeukeng.jessie@yahoo.com>
2024-05-09 11:39:08 +0200

    Parser Klasse wurde angepasst

--- Parser.java
+++ Parser.java
@@ -9,20 +9,11 @@ public class Parser {
         this.tokens = tokens;
     }
 
-    /**parse(): This is the entry point of the parser. 
-     * It calls the expression() method, which starts the parsing process. 
-     * It returns a Node object, which is the result of parsing the input tokens. */
     public Node parse() {
         Node node = expression();
         return node;
     }
 
-    /**expression(): This method is used to parse expressions in the input. 
-     * It starts by calling the term() method to get the nodeLeft operand, then it looks for any plus or minus operators, 
-     * and if found, it calls the term() method again to get the nodeRight operand. 
-     * It then creates a new TwoChildNode object with the nodeLeft and nodeRight operands and the operator and 
-     * sets it as the nodeLeft operand for the next iteration. 
-     * It returns the final Node object that represents the parsed expression. */
     private Node expression() {
         Node nodeLeft = term();
         while (match(TYPE.PLUS, TYPE.MINUS)) {
@@ -33,8 +24,6 @@ public class Parser {
         return nodeLeft;
     }
 
-    /**term(): This method is used to parse terms in the input. 
-     * It works similar to the expression() method but it looks for any multiply or divide operators. */
     private Node term() {
         Node nodeLeft = factor();
         while (match(TYPE.MULT, TYPE.DIV)) {
@@ -45,12 +34,6 @@ public class Parser {
         return nodeLeft;
     }
 
-    /**factor(): This method is used to parse factors in the input. 
-     * It starts by checking if the current token is a unary operator (plus or minus), if yes, 
-     * it calls the factor() method again to get the nodeRight operand and creates a new OneChildNode object with the operator 
-     * and the nodeRight operand. If the current token is an integer or float, it creates a new ValueNode object with the 
-     * token's value. If the current token is a nodeLeft parenthesis, it calls the expression() method to get the 
-     * expression inside the parenthesis and returns it. */
    private Node factor() {
         if (match(TYPE.PLUS, TYPE.MINUS)) {
             Token operator = getPreviousToken();
