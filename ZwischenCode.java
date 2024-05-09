ac04b727fe5049822a6977a13297293bfc549930
Jessieca29 <zeukeng.jessie@yahoo.com>
2024-05-09 11:29:55 +0200

    ZwischenCode Klasse wurde angespasst.

--- ZwischenCode.java
+++ ZwischenCode.java
@@ -2,37 +2,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ZwischenCode {
-    private Node root;
-    private List<String> intermediateCode;
+    private Node parentNode;
+    private List<String> zwischenCode;
 
     public ZwischenCode(Node root) {
-        this.root = root;
-        this.intermediateCode = new ArrayList<>();
+        this.parentNode = root;
+        this.zwischenCode = new ArrayList<>();
     }
 
     public List<String> generate() {
-        generate(root);
-        return intermediateCode;
+        erzeugen(parentNode);
+        return zwischenCode;
     }
 
-    private void generate(Node node) {
+    private void erzeugen(Node node) {
         if (node instanceof ValueNode) {
             ValueNode valueNode = (ValueNode) node;
-            intermediateCode.add("PUSH" + "\t\t" + valueNode.value.value);
+            zwischenCode.add("PUSH" + "\t\t" + valueNode.value.value);
         } else if (node instanceof OneChildNode) {
             OneChildNode OneChildNode = (OneChildNode) node;
-            generate(OneChildNode.nodeRight);
-            intermediateCode.add("OPERATOR" + "\t" + OneChildNode.operator.value);
+            erzeugen(OneChildNode.nodeRight);
+            zwischenCode.add("OPERATOR" + "\t" + OneChildNode.operator.value);
         } else if (node instanceof TwoChildNode) {
             TwoChildNode TwoChildNode = (TwoChildNode) node;
-            generate(TwoChildNode.nodeLeft);
-            generate(TwoChildNode.nodeRight);
-            intermediateCode.add("OPERATOR" + "\t" + TwoChildNode.operator.value);
+            erzeugen(TwoChildNode.nodeLeft);
+            erzeugen(TwoChildNode.nodeRight);
+            zwischenCode.add("OPERATOR" + "\t" + TwoChildNode.operator.value);
         } 
     }
 
     public void print() {
-        for (String line : intermediateCode) {
+        for (String line : zwischenCode) {
             System.out.println(line);
         }
     }
